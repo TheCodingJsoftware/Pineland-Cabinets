20ee51c8cc0b38e5354c02ef0818f4e28ae49d08
JareBear <jaredgrozz@gmail.com>
2022-08-21 00:38:07 -0500

    pics

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-"""
-This script is seperate from the entire client-end project
-and is not intended for the client to use this script.
-"""
+import contextlib
+import glob
+import re
+import unicodedata
+from ast import Index
 
 from flask import Flask, render_template
 
@@ -18,7 +19,63 @@ def index() -> None:
     Returns:
       The index.html file
     """
-    return render_template("index.html")
+    print(get_all_folders())
+    return render_template(
+        "index.html", folders=get_all_folders(), pictures=get_all_images()
+    )
+
+
+def get_all_images() -> dict:
+    pictures = {"All": []}
+    for filename in glob.iglob("static/pictures" + "**/**", recursive=True):
+        if "." in filename:
+            folder_name = slugify(filename.split("\\")[-2]).replace(
+                "staticpictures", ""
+            )
+            pictures[folder_name] = []
+    for filename in glob.iglob("static/pictures" + "**/**", recursive=True):
+        if "." in filename:
+            folder_name = slugify(filename.split("\\")[-2]).replace(
+                "staticpictures", ""
+            )
+            pictures["All"].append(filename)
+            pictures[folder_name].append(filename)
+    return pictures
+
+
+def get_all_folders() -> dict:
+    folder_names = {"All": "All"}
+    with contextlib.suppress(IndexError):
+        for filename in glob.iglob("static/pictures" + "**/**", recursive=False):
+            folder_names[
+                slugify(filename.split("/")[-1]).replace("staticpictures", "")
+            ] = (
+                filename.split("/")[-1]
+                .replace("static\\", "")
+                .replace("pictures\\", "")
+            )
+    return folder_names
+
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
 
 
 # app.run(host="10.0.0.217", port=5000, debug=False, threaded=True)
